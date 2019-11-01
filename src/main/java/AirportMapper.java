3403bd6aa14e47cc73d13825983c0e068f51ec0b
stepanbog <step300699@yandex.ru>
2019-11-02 00:21:22 +0300

    Scripted auto-commit on change (2019-11-02 00:21:22) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -1,3 +1,12 @@
-public class AirportMapper {
-    pri
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.map.WrappedMapper;
+
+
+public class AirportMapper  extends Mapper<IntWritable, Text,AirportKey,Text> {
+    protected void map(IntWritable key, Text value, Context context){
+        
+    }
+
 }
