e00ea150cb4e58b85aff7c5cc1dee43b21663c1a
stepanbog <step300699@yandex.ru>
2019-11-02 00:04:38 +0300

    Scripted auto-commit on change (2019-11-02 00:04:38) by gitwatch.sh

--- src/main/java/Lab2.java
+++ src/main/java/Lab2.java
@@ -17,6 +17,7 @@ public class Lab2{
         job.setJarByClass(Lab2.class);
         job.setJobName("Lab2");
         FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileInputFormat.addInputPath(job, new Path(args[1]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.setMapperClass(WordMapper.class);
         job.setReducerClass(WordReducer.class);
