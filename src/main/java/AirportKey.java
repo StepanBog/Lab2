103ac407eebf767d22be141690e1e198dd2168f5
stepanbog <step300699@yandex.ru>
2019-11-02 00:37:21 +0300

    Scripted auto-commit on change (2019-11-02 00:37:21) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -1,2 +1,24 @@
-public class AirportKey {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportKey  implements WritableComparable<AirportKey> {
+    private IntWritable nummer;
+    private Text airportValue;
+
+    public int compareTo(AirportKey airportKey) {
+        return 0;
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
