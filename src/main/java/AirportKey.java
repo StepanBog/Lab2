ae35458018f5857ec116cc7dcf5f339fd9e5f329
stepanbog <step300699@yandex.ru>
2019-11-02 00:45:26 +0300

    Scripted auto-commit on change (2019-11-02 00:45:26) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -7,18 +7,32 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportKey  implements WritableComparable<AirportKey> {
-    private IntWritable nummer;
+    private IntWritable number;
     private Text airportValue;
 
+    public AirportKey(String value, int number){
+        airportValue = new Text(value);
+        this.number = new IntWritable(number);
+    }
+
     public int compareTo(AirportKey airportKey) {
-        return 0;
+        if (airportKey.airportValue.compareTo(this.airportValue) == 0)
+            return airportKey.number.compareTo(this.number);
+        else
+            return airportKey.airportValue.compareTo(this.airportValue);
     }
 
     public void write(DataOutput dataOutput) throws IOException {
-
+        airportValue.write(dataOutput);
+        number.write(dataOutput);
     }
 
     public void readFields(DataInput dataInput) throws IOException {
-
+        airportValue.readFields(dataInput);
+        number.readFields(dataInput);
     }
+    public Text getAirportValue(){
+        return airportValue;
+    }
+
 }
