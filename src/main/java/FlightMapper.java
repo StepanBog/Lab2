f2379abf2a0516bce93e73a7a65ea8256a1402cd
stepanbog <step300699@yandex.ru>
2019-11-02 01:03:27 +0300

    Scripted auto-commit on change (2019-11-02 01:03:27) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,4 +1,15 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.map.WrappedMapper;
 
-public class FlightMapper extends Mapper {
+import java.io.IOException;
+
+
+public class FlightMapper  extends Mapper<IntWritable, Text,AirportKey,Text> {
+    protected void map(IntWritable key, Text value, Context context) throws IOException, InterruptedException{
+        String[] words = value.toString().split(",[\"]");
+        context.write(new AirportKey(words[0].replace("\"",""),0), new  Text(words[1].replace("\"","")));
+    }
+
 }
