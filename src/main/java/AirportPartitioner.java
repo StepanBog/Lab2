7490ea6ef9e62cf3e5be42d6d8abbe9f4669ab45
stepanbog <step300699@yandex.ru>
2019-11-02 08:55:20 +0300

    Scripted auto-commit on change (2019-11-02 08:55:20) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -2,5 +2,5 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportKey, Text> {
-    public int getPartition(AirportKey key, )
+    public int getPartition(AirportKey key, Text value, int num )
 }
