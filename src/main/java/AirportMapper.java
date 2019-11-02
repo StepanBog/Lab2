66c5f4a32c18d8d84df62fe6cdaf941a9f88715b
stepanbog <step300699@yandex.ru>
2019-11-02 09:02:20 +0300

    Scripted auto-commit on change (2019-11-02 09:02:20) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 public class AirportMapper  extends Mapper<IntWritable, Text,AirportKey,Text> {
     protected void map(IntWritable key, Text value, Context context) throws IOException, InterruptedException{
         String[] words = value.toString().split(",[\"]");
+        String airportValue = words[0].replace("\"","");
+        String a
         context.write(new AirportKey(words[0].replace("\"",""),0), new  Text(words[1].replace("\"","")));
     }
 
