f13aeac9ba4bda508b1772b992a234c28336c459
stepanbog <step300699@yandex.ru>
2019-11-02 08:56:50 +0300

    Scripted auto-commit on change (2019-11-02 08:56:50) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -5,7 +5,7 @@ import static java.sql.JDBCType.INTEGER;
 
 public class AirportPartitioner extends Partitioner<AirportKey, Text> {
     public int getPartition(AirportKey key, Text value, int numReduceTask ){
-        return (key.getAirportValue().hashCode() & INTEGER.)
+        return (key.getAirportValue().hashCode() & Integer.MAX_VALUE)
     }
 
 }
