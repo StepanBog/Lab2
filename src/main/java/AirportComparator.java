cde6bced034a5147e2db53d789d4a64299b9a963
stepanbog <step300699@yandex.ru>
2019-11-02 09:00:51 +0300

    Scripted auto-commit on change (2019-11-02 09:00:50) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -1,5 +1,12 @@
 import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-public class AirportComparator implements WritableComparable {
+public class AirportComparator extends WritableComparator {
+    public int compare(WritableComparable a,WritableComparable b){
+        AirportKey A = (AirportKey) a;
+        AirportKey B = (AirportKey) b;
+        return A.getAirportValue().compareTo(B.getAirportValue());
+
+    }
 }
